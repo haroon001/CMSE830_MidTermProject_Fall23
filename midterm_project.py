e90a57c27f00716a1f9b67a0c9ca0a9e2fdbac61
haroon001 <ranaharoong14@gmail.com>
2023-10-13 00:29:32 -0400

    dataset & initial script

---
+++ midterm_project.py
@@ -0,0 +1,26 @@
+import streamlit as st
+import pandas as pd
+import seaborn as sns
+import json
+import numpy as np
+
+def load_csv(csv_path, save = False):
+	with pd.read_csv(csv_path, iterator=True) as reader:
+		chunk_df = reader.get_chunk(10000)	
+		chunk_df.sort_values(by='date', inplace = True)
+		print(chunk_df['date'].iloc[0], chunk_df['date'].iloc[5000], chunk_df['date'].iloc[-1])
+		
+		if save:
+			chunk_df.to_csv('/home/builder/Downloads/CMSE/gcrp_50K.csv', index=False)
+
+	return chunk_df
+
+
+if __name__=='__main__':
+
+	path = 'gcrp_5K.csv'
+	df = pd.read_csv(path)
+	df.dropna(inplace = True)
+
+	st.write("Users distribution globally.")
+	st.map(df, latitude='lat', longitude='long')
