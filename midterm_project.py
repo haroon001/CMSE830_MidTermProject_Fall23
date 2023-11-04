d9c5d2822e78cc70764b77cc091d370d3728c56f
haroon001 <ranaharoong14@gmail.com>
2023-11-03 23:52:07 -0400

    text cleaning

--- midterm_project.py
+++ midterm_project.py
@@ -198,10 +198,12 @@ if __name__=='__main__':
 	st.pyplot(fig)
 	st.divider()
 	
-	st.subheader("Parallel Plot for Revenue")
+	st.subheader("Parallel Plot for Revenue(Effective with white background!)")
 	st.write("Here parallel plot highlights key factor in generating revenue which is total time spent \
-		on online store by user, higher the time higher the odds of revenue. It suggests that we should \
-		also spend and focus on user interface, user experience and other factors which can keep visitor engaged on site.")
+		on online store by user, higher the spent time higher the odds of revenue (darker lines). It suggests that we should \
+		also spend and focus on user interface, user experience and other factors which can keep visitor engaged on site. \
+		Very high values for spent time should be ignored as they likely represent data scrapping bots in most cases, \
+		safe range is less than or equal to 2000.")
 
 	fig = px.parallel_coordinates(subset_df[['Date', 'VisitorId','TimeOnSite', 'Transaction Revenue']], color="Transaction Revenue",color_continuous_scale=px.colors.diverging.Tealrose, color_continuous_midpoint=2)
 	st.plotly_chart(fig)
